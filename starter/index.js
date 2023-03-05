412ee03fd93238236d0a2dfe7265d3be84c81f44
Abayomi Balogun <109353177+abayomi2010@users.noreply.github.com>
2023-03-05 21:43:15 +0000

    added prompts for manager's details and option to add an engineer, intern or finish building the team

--- starter/index.js
+++ starter/index.js
@@ -16,6 +16,79 @@ const teamMembers = [];
 const idList = [];
 
 const appMenu = () => {
+    function buildTeam() {
+
+    }
+
+    function addIntern () {
+
+    }
+
+
+    function addEngineer() {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "engineerName",
+                message: "What is your engineer name ?",
+            },
+
+            {
+                type: "input",
+                name: "engineerId",
+                message: "What is your engineer id?",
+            }, 
+
+            {
+                type: "input",
+                name: "engineerEmail",
+                message: "What is your engineer email?",
+            }, 
+
+            {
+                type: "input",
+                name: "engineerGithub",
+                message: "What is your engineer github?",
+            }
+        ]).then (answers => {
+            const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
+            teamMembers.push(engineer);
+            idList.push(answers.engineerId);
+            console.log(engineer);
+            createTeam();
+        })
+    }
+
+
+    function createTeam () {
+        inquirer.prompt([
+            {
+                type: "list",
+                name: "memberChoice",
+                message: "What type of team member would you like to add?",
+                choices: [
+                    "Engineer",
+                    "Intern",
+                    "I don't want to add any more team members"
+                ]
+            }
+        ]).then(userChoice => {
+            if (userChoice.memberChoice === "Engineer") {
+                // Add Engineer to teamMembers array
+                addEngineer();
+            } else if (userChoice.memberChoice === "Intern") {
+                // Add Intern to teamMembers array
+                addIntern();
+            } else {
+                // build team function
+                buildTeam();
+            }
+        })
+    }
+
+
+
+
     function createManager() {
         console.log("Please build your team")
         inquirer.prompt([
@@ -63,17 +136,19 @@ const appMenu = () => {
                     if (answer!== "") {
                         return true;
                     }
-                    return "Please enter a name";
+                    return "Please enter a number";
                 }
             }
 
         ]).then(answers => {
-            const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber)
+            const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
+            console.log(manager);
             teamMembers.push(manager);
-            idList.push(answers.managerId);  
+            idList.push(answers.managerId);
+            createTeam();  
         });
     }
     createManager();
 }
 
-appMenu();
+appMenu(); 
